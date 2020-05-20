6488bd4ea74ab2e19b8af8a9df7bd2c4ec4c0a3a
hcan <hcan@trinitysoft.co.kr>
2020-05-20 09:41:58 +0900

    초기 파일.

---
+++ CliNet/ResponseObserver.cs
@@ -0,0 +1,25 @@
+﻿using AustinHarris.JsonRpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliNet
+{
+    public class ResponseObserver : IObserver<JsonResponse<int>>
+    {
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(JsonResponse<int> value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
