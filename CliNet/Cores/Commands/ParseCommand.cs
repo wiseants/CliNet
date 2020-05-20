1505fcaf83306fd738eac71485a970fe72e3d5ad
hcan <hcan@trinitysoft.co.kr>
2020-05-20 10:21:12 +0900

    파싱 커맨드 추가.

--- CliNet/Cores/Commands/ParseCommand.cs
+++ CliNet/Cores/Commands/ParseCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using CliNet.Interfaces;
+using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace CliNet.Cores.Commands
 {
-    class ParseCommand
+    public class ParseCommand : IAction
     {
+        public bool IsValid => true;
+
+        /// <summary>
+        /// 필수 옵션.
+        /// </summary>
+        [Option('f', "file", Required = true, HelpText = "file-name.")]
+        public string FileFullPath
+        {
+            get;
+            set;
+        }
+
+        public int Action()
+        {
+            var target = new AntlrInputStream(FileFullPath);
+            var lexer = new CSharpLexer(target);
+            var tokens = new CommonTokenStream(lexer);
+            CSharpParser parser = new CSharpParser(tokens) { BuildParseTree = true };
+
+            CSharpParser.LiteralContext result = parser.literal();
+
+            Console.WriteLine(result.ToStringTree());
+
+            return 1;
+        }
     }
 }
