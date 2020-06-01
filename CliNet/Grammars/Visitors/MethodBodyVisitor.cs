03366e7d11215a0b59f2f0b5d02fd38097506d6a
hcan <hcan@trinitysoft.co.kr>
2020-06-01 16:55:16 +0900

    C++ 그래마 추가.

---
+++ CliNet/Grammars/Visitors/MethodBodyVisitor.cs
@@ -0,0 +1,22 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliNet.Grammars.Visitors
+{
+    public class MethodBodyVisitor : AbstractParseTreeVisitor<bool>
+    {
+        public MethodBodyVisitor()
+        {
+        }
+
+        public override bool Visit([NotNull] IParseTree tree)
+        {
+            return true;
+        }
+    }
+}
