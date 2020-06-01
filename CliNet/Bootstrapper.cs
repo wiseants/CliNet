a00eb08e64e821e7f199bfc57a00022d3f823408
hcan <hcan@trinitysoft.co.kr>
2020-06-01 18:00:21 +0900

    브트스르랩 파일 추가.

---
+++ CliNet/Bootstrapper.cs
@@ -0,0 +1,46 @@
+﻿namespace CliNet
+{
+    public class Bootstrapper
+    {
+        #region Fileds
+
+        private static Bootstrapper instance = null;
+
+        #endregion
+
+        #region Constructors
+
+        private Bootstrapper()
+        {
+            BuildContainer();
+        }
+
+        #endregion
+
+        #region Properties
+
+        private static Bootstrapper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Bootstrapper();
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void BuildContainer()
+        {
+
+        }
+
+        #endregion
+    }
+}
