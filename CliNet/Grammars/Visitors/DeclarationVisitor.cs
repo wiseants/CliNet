bedc3abed46cdce059061e01f49a5dc48071aa22
hcan <hcan@trinitysoft.co.kr>
2020-07-02 16:53:50 +0900

    C# 그래마 추가.

---
+++ CliNet/Grammars/Visitors/DeclarationVisitor.cs
@@ -0,0 +1,38 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using CliNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CliNet.CPP14.CPP14Parser;
+
+namespace CliNet.Grammars.Visitors
+{
+    public class DeclarationVisitor : AbstractParseTreeVisitor<bool>
+    {
+        public DeclarationVisitor()
+        {
+
+        }
+
+        public List<VariableInfo> VariableInfoList
+        {
+            get;
+        } = new List<VariableInfo>();
+
+        public override bool VisitChildren([NotNull] IRuleNode node)
+        {
+            if (node is DeclarationstatementContext declarationstatementContext)
+            {
+                DeclarationStatementVisitor declarationStatementVisitor = new DeclarationStatementVisitor();
+                declarationStatementVisitor.Visit(declarationstatementContext);
+
+                VariableInfoList.AddRange(declarationStatementVisitor.VariableInfoList);
+            }
+
+            return base.VisitChildren(node);
+        }
+    }
+}
