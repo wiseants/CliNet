844d05f6da0b20bd53400cf614ecf5c65dacf120
hcan <wiseant17@gmail.com>
2023-11-02 11:35:57 +0900

    서버 수정.

--- CliNet/Cores/Commands/PrintServerCommand.cs
+++ CliNet/Cores/Commands/PrintServerCommand.cs
@@ -1,11 +1,15 @@
 ﻿using AustinHarris.JsonRpc;
 using CliNet.Interfaces;
+using CliNet.Models;
 using CommandLine;
 using Common.Network;
+using Elasticsearch.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +19,8 @@ namespace CliNet.Cores.Commands
     [Verb("print", HelpText = "입력되는 문자열을 출력하는 서버 시작.")]
     internal class PrintServerCommand : IAction
     {
+        private Socket _sock;
+
         public bool IsValid => true;
 
         [Option('p', "port", Required = false, HelpText = "서버 포트 번호.")]
@@ -26,15 +32,43 @@ namespace CliNet.Cores.Commands
 
         public int Action()
         {
-            SocketListener socketListener = new SocketListener(IPAddress.Parse("127.0.0.1"), Port);
-            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            _sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, Port);
 
-            socketListener.StartAsync((writer, line) =>
-            {
-                Console.WriteLine(line);
-            }, tokenSource.Token);
+            _sock.Bind(serverEP);
+            _sock.Listen(10);
+            _sock.BeginAccept(AcceptCallback, null);
 
             return 0;
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = _sock.EndAccept(ar);
+                AsyncObject obj = new AsyncObject(1920 * 1080 * 3);
+                obj.WorkingSocket = client;
+                client.BeginReceive(obj.Buffer, 0, 1920 * 1080 * 3, 0, DataReceived, obj);
+
+                _sock.BeginAccept(AcceptCallback, null);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            AsyncObject obj = (AsyncObject)ar.AsyncState;
+
+            int received = obj.WorkingSocket.EndReceive(ar);
+
+            byte[] buffer = new byte[received];
+
+            Array.Copy(obj.Buffer, 0, buffer, 0, received);
+
+            Console.WriteLine(Encoding.Default.GetString(buffer));
         }
     }
 }
