f632e48addb481329939d39e664612927a29af0c
hcan <wiseant17@gmail.com>
2023-12-13 11:48:37 +0900

    시퀀스 번호를 위한 설정 추가.

--- CliNet/Cores/Managers/SequenceManager.cs
+++ CliNet/Cores/Managers/SequenceManager.cs
@@ -1,34 +1,50 @@
 ﻿using Common.Templates;
+using Common.Tools;
 using System;
-using System.Configuration;
 
 namespace CliNet.Cores.Managers
 {
+    /// <summary>
+    /// 시퀀스 번호 관리자.
+    /// </summary>
     public class SequenceManager : Singleton<SequenceManager>
     {
+        private readonly string SEQ_KEY = "LastSeqNo";
+
         #region Properties
 
+        /// <summary>
+        /// 현재 시퀀스 번호.
+        /// </summary>
         public int CurrentSeqNo
         {
-            get => Convert.ToInt32(ConfigurationManager.AppSettings["LastSeqNo"]);
+            get => Convert.ToInt32(AppConfiguration.GetAppConfig(SEQ_KEY));
         }
 
         #endregion
 
         #region Public methods
 
+        /// <summary>
+        /// 다음 시퀀스 번호.
+        /// 자동으로 시퀀스 번호는 증가.
+        /// </summary>
+        /// <returns></returns>
         public int GetNext()
         {
             int nextSeqNo = CurrentSeqNo + 1;
 
-            ConfigurationManager.AppSettings["LastSeqNo"] = nextSeqNo.ToString();
+            AppConfiguration.SetAppConfig(SEQ_KEY, nextSeqNo.ToString());
 
             return nextSeqNo;
         }
 
+        /// <summary>
+        /// 시퀀스 번호 초기화.
+        /// </summary>
         public void Reset()
         {
-            ConfigurationManager.AppSettings["LastSeqNo"] = "0";
+            AppConfiguration.SetAppConfig(SEQ_KEY, 0.ToString());
         }
 
         #endregion
---
+++ Common/Tools/AppConfiguration.cs
@@ -0,0 +1,81 @@
+﻿// https://blog.kgoon.net/4
+using System.Configuration;
+
+namespace Common.Tools
+{
+    /// <summary>
+    /// App.config 사용 도구.
+    /// </summary>
+    public class AppConfiguration
+    {
+        #region Public methods
+
+        /// <summary>
+        /// 설정 값 획득.
+        /// </summary>
+        /// <param name="key">설정 키워드.</param>
+        /// <returns>설정 값.</returns>
+        public static string GetAppConfig(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        /// <summary>
+        /// 설정 값 대입.
+        /// </summary>
+        /// <param name="key">설정 키워드.</param>
+        /// <param name="value">대입할 값.</param>
+        public static void SetAppConfig(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection cfgCollection = config.AppSettings.Settings;
+
+            cfgCollection.Remove(key);
+            cfgCollection.Add(key, value);
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+        }
+
+        /// <summary>
+        /// 설정 추가.
+        /// </summary>
+        /// <param name="key">설정 키워드.</param>
+        /// <param name="value">최초 값.</param>
+        public static void AddAppConfig(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            KeyValueConfigurationCollection cfgCollection = config.AppSettings.Settings;
+
+            cfgCollection.Add(key, value);
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+        }
+
+        /// <summary>
+        /// 설정 제거.
+        /// </summary>
+        /// <param name="key">설정 키워드.</param>
+        public static void RemoveAppConfig(string key)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            KeyValueConfigurationCollection cfgCollection = config.AppSettings.Settings;
+
+            try
+            {
+                cfgCollection.Remove(key);
+
+                config.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+            }
+            catch { }
+        }
+
+        #endregion
+    }
+}
