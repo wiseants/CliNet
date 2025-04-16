4fd44affb92372f88a924ce4ddf09b15e7f110a4
hcan <wiseant17@gmail.com>
2025-04-16 17:52:36 +0900

    반복 기능 추가.

--- CliNet/Cores/Commands/Gcs/GetServerStateCommand.cs
+++ CliNet/Cores/Commands/Gcs/GetServerStateCommand.cs
@@ -44,6 +44,13 @@ namespace CliNet.Cores.Commands.Gcs
             set;
         } = 15300;
 
+        [Option('c', "counter", Required = false, HelpText = "반복 요청 횟수.")]
+        public int Counter
+        {
+            get;
+            set;
+        } = 1;
+
         [Option('t', "timeout", Required = false, HelpText = "타임아웃 시간(ms)")]
         public int Timeout
         {
@@ -59,14 +66,9 @@ namespace CliNet.Cores.Commands.Gcs
         {
             try
             {
-                while (true)
+                using (TcpClient client = new TcpClient(IpAddress, Port))
                 {
-                    using (TcpClient client = new TcpClient(IpAddress, Port))
-                    {
-                        RequestAndListen(client);
-                    }
-
-                    Thread.Sleep(1000);
+                    RequestAndListen(client);
                 }
             }
             catch (Exception ex)
@@ -85,19 +87,25 @@ namespace CliNet.Cores.Commands.Gcs
         {
             using (NetworkStream stream = client.GetStream())
             {
-                GetServerStateInfo requestInfo = new GetServerStateInfo();
+                int i = 0;
+                while (Counter > i++)
+                {
+                    GetServerStateInfo requestInfo = new GetServerStateInfo();
 
-                string requestString = JsonConvert.SerializeObject(requestInfo);
-                Console.WriteLine($"서버로 보내는 요청:\n {requestString}");
+                    string requestString = JsonConvert.SerializeObject(requestInfo);
+                    Console.WriteLine($"서버로 보내는 요청:\n {requestString}");
 
-                byte[] sendBuffer = Encoding.Default.GetBytes(requestString);
-                stream.Write(sendBuffer, 0, sendBuffer.Length);
+                    byte[] sendBuffer = Encoding.Default.GetBytes(requestString);
+                    stream.Write(sendBuffer, 0, sendBuffer.Length);
 
-                byte[] receivedBuffer = new byte[BUFFER_SIZE];
-                int receivedLength = stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+                    byte[] receivedBuffer = new byte[BUFFER_SIZE];
+                    int receivedLength = stream.Read(receivedBuffer, 0, receivedBuffer.Length);
 
-                string responseString = Encoding.Default.GetString(receivedBuffer, 0, receivedLength);
-                Console.WriteLine($"서버로부터 받은 응답:\n {responseString}");
+                    string responseString = Encoding.Default.GetString(receivedBuffer, 0, receivedLength);
+                    Console.WriteLine($"서버로부터 받은 응답:\n {responseString}");
+
+                    Thread.Sleep(1000);
+                }
             }
         }
 
