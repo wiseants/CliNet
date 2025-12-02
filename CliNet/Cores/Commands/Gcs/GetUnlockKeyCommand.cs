092176ade3b7b8d9f868ff2610c05582bbb475e0
hcan <wiseant17@gmail.com>
2025-12-02 13:09:01 +0900

    해제 키 생성 명령 추가.

---
+++ CliNet/Cores/Commands/Gcs/GetUnlockKeyCommand.cs
@@ -0,0 +1,59 @@
+﻿using CommandLine;
+using Common.Tools;
+using System;
+
+namespace CliNet.Cores.Commands.Gcs
+{
+    [Verb("get.unlock", HelpText = "GCS 상태 정보 요청.")]
+    internal class GetUnlockKeyCommand : Interfaces.IAction
+    {
+        #region Fields
+
+        private readonly int BUFFER_SIZE = 1024;
+
+        #endregion
+
+        #region Constructors
+
+        public GetUnlockKeyCommand() 
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsValid => true;
+
+        [Option('k', "key", Required = true, HelpText = "입력 초기화 키.")]
+        public string ResetKey
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public int Action()
+        {
+            bool isSuccess = false;
+            ushort unlockKey = default;
+
+            try
+            {
+                unlockKey = KeyTool.GetSimpleHash(Convert.ToUInt16(ResetKey, 16));
+
+                isSuccess = true;
+            }
+            catch { }
+
+            Console.WriteLine(isSuccess ? $"해제 키: {unlockKey:X4}" : "생성 실패");
+
+            return 0;
+        }
+
+        #endregion
+    }
+}
---
+++ Common/Tools/KeyTool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Common.Tools
+{
+    /// <summary>
+    /// From Gemini
+    /// </summary>
+    public class KeyTool
+    {
+        /// <summary>
+        /// ushort 범위의 난수를 반환합니다.
+        /// </summary>
+        /// <param name="minValue">최소 값.</param>
+        /// <param name="maxValue">최대 값.</param>
+        /// <returns>생성된 난수.</returns>
+        public static ushort GetRandomUShort(int minValue, int maxValue)
+        {
+            if (minValue < ushort.MinValue) minValue = ushort.MinValue; // 0
+            if (maxValue > ushort.MaxValue + 1) maxValue = ushort.MaxValue + 1; // 65536
+
+            Random random = new Random();
+
+            return (ushort)random.Next(minValue, maxValue);
+        }
+
+        /// <summary>
+        /// ushort 전체 범위의 난수 생성.
+        /// </summary>
+        /// <returns>생성된 난수.</returns>
+        public static ushort GetRandomUShort()
+        {
+            return GetRandomUShort(ushort.MinValue, ushort.MaxValue + 1);
+        }
+
+        /// <summary>
+        /// 간단한 해시 생성.
+        /// </summary>
+        /// <param name="input">해시 입력 값.</param>
+        /// <returns>해시 출력 값.</returns>
+        public static ushort GetSimpleHash(ushort input)
+        {
+            ushort shifted = (ushort)(input << 8 | input >> 8);
+
+            ushort result = (ushort)(input ^ shifted ^ 0xDEAD);
+
+            return result;
+        }
+    }
+}
